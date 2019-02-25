5e13d382381b1e7d0ecd90b2f92ec4457611cfe3
Vadim Yakovets <46343332+Vex788@users.noreply.github.com>
2019-02-25 18:02:41 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Dithering
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._PictureBox1 = new System.Windows.Forms.PictureBox();
+            this._PictureBox2 = new System.Windows.Forms.PictureBox();
+            this._ButtonImage = new MetroFramework.Controls.MetroButton();
+            this._ButtonDithering = new MetroFramework.Controls.MetroButton();
+            this._ProgressBar = new MetroFramework.Controls.MetroProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this._PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._PictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _PictureBox1
+            // 
+            this._PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._PictureBox1.Location = new System.Drawing.Point(23, 100);
+            this._PictureBox1.Name = "_PictureBox1";
+            this._PictureBox1.Size = new System.Drawing.Size(373, 250);
+            this._PictureBox1.TabIndex = 0;
+            this._PictureBox1.TabStop = false;
+            // 
+            // _PictureBox2
+            // 
+            this._PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._PictureBox2.Location = new System.Drawing.Point(402, 100);
+            this._PictureBox2.Name = "_PictureBox2";
+            this._PictureBox2.Size = new System.Drawing.Size(373, 250);
+            this._PictureBox2.TabIndex = 1;
+            this._PictureBox2.TabStop = false;
+            // 
+            // _ButtonImage
+            // 
+            this._ButtonImage.Location = new System.Drawing.Point(287, 379);
+            this._ButtonImage.Name = "_ButtonImage";
+            this._ButtonImage.Size = new System.Drawing.Size(109, 23);
+            this._ButtonImage.Style = MetroFramework.MetroColorStyle.Blue;
+            this._ButtonImage.TabIndex = 2;
+            this._ButtonImage.Text = "Image";
+            this._ButtonImage.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this._ButtonImage.UseSelectable = true;
+            this._ButtonImage.Click += new System.EventHandler(this._ButtonImage_Click);
+            // 
+            // _ButtonDithering
+            // 
+            this._ButtonDithering.Location = new System.Drawing.Point(402, 379);
+            this._ButtonDithering.Name = "_ButtonDithering";
+            this._ButtonDithering.Size = new System.Drawing.Size(109, 23);
+            this._ButtonDithering.Style = MetroFramework.MetroColorStyle.Blue;
+            this._ButtonDithering.TabIndex = 3;
+            this._ButtonDithering.Text = "Dithering";
+            this._ButtonDithering.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this._ButtonDithering.UseSelectable = true;
+            this._ButtonDithering.Click += new System.EventHandler(this._ButtonDithering_Click);
+            // 
+            // _ProgressBar
+            // 
+            this._ProgressBar.Location = new System.Drawing.Point(484, 225);
+            this._ProgressBar.Name = "_ProgressBar";
+            this._ProgressBar.Size = new System.Drawing.Size(221, 10);
+            this._ProgressBar.Style = MetroFramework.MetroColorStyle.Blue;
+            this._ProgressBar.TabIndex = 8;
+            this._ProgressBar.Theme = MetroFramework.MetroThemeStyle.Dark;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 445);
+            this.Controls.Add(this._ProgressBar);
+            this.Controls.Add(this._ButtonDithering);
+            this.Controls.Add(this._ButtonImage);
+            this.Controls.Add(this._PictureBox2);
+            this.Controls.Add(this._PictureBox1);
+            this.Name = "Form1";
+            this.Text = "Dithering";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this._PictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._PictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox _PictureBox1;
+        private System.Windows.Forms.PictureBox _PictureBox2;
+        private MetroFramework.Controls.MetroButton _ButtonImage;
+        private MetroFramework.Controls.MetroButton _ButtonDithering;
+        private MetroFramework.Controls.MetroProgressBar _ProgressBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Dithering
+{
+    public partial class Form1 : MetroForm
+    {
+        private string pathToImage = null;
+        private Thread ditheringImageThread;
+
+        private int saturated_add(int val1, int val2)
+        {
+            int val1_int = val1;
+            int val2_int = val2;
+            int tmp = val1_int + val2_int;
+
+            if (tmp > 255)
+            {
+                return 255;
+            }
+            else if (tmp < 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return tmp;
+            }
+        }
+
+        private void DitheringImage()
+        {
+            Bitmap bitmap = _PictureBox1.Image as Bitmap;
+            Bitmap modifiedBitmap = _PictureBox1.Image as Bitmap;
+
+            Invoke((Action)(() =>
+            {
+                _ProgressBar.Maximum = bitmap.Size.Width * bitmap.Size.Height - 1;
+                _ProgressBar.Show();
+
+                int[][] buffer = new int[bitmap.Size.Height][];
+
+                for (int i = 0; i < bitmap.Size.Height; i++)
+                    buffer[i] = new int[bitmap.Size.Width];
+
+                for (int y = 0; y < bitmap.Size.Height; y++)
+                {
+                    for (int x = 0; x < bitmap.Size.Width; x++)
+                    {
+                        buffer[y][x] = bitmap.GetPixel(x, y).R;
+                    }
+                }
+
+                int err = 0, a = 0, b = 0, c = 0, d = 0;
+
+                for (int y = 0; y < bitmap.Size.Height; y++)
+                {
+                    err = 0;
+
+                    for (int x = 0; x < bitmap.Size.Width; x++)
+                    {
+                        _ProgressBar.Value += 1;
+
+                        buffer[y][x] = bitmap.GetPixel(x, y).R;
+
+                        if ((bitmap.GetPixel(x, y).R + buffer[y][x]) - err > 127)
+                        {
+                            err = 255 - bitmap.GetPixel(x, y).R + buffer[y][x];
+                            bitmap.SetPixel(x, y, Color.White);
+                        }
+                        else
+                        {
+                            err = 0 - bitmap.GetPixel(x, y).R + buffer[y][x];
+                            bitmap.SetPixel(x, y, Color.Black);
+                        }
+
+                        a = (err * 7) / 16;
+                        b = (err * 1) / 16;
+                        c = (err * 5) / 16;
+                        d = (err * 3) / 16;
+
+                        if ((y != 0) && (y != (bitmap.Size.Height - 1)) && (x != 0) && (x != (bitmap.Size.Width - 1)))
+                        {
+                            buffer[y + 0][x + 1] = saturated_add(bitmap.GetPixel(x + 0, y + 1).R + buffer[y + 0][x + 1], a);
+                            buffer[y + 1][x + 1] = saturated_add(bitmap.GetPixel(x + 1, y + 1).R + buffer[y + 1][x + 1], b);
+                            buffer[y + 1][x + 0] = saturated_add(bitmap.GetPixel(x + 1, y + 0).R + buffer[y + 1][x + 0], c);
+                            buffer[y + 1][x - 1] = saturated_add(bitmap.GetPixel(x + 1, y - 1).R + buffer[y + 1][x - 1], d);
+                        }
+                    }
+                }
+
+                _ProgressBar.Hide();
+                _ProgressBar.Value = 0;
+                _PictureBox2.Image = bitmap;
+            }));
+
+            ditheringImageThread.Abort();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _PictureBox1.Load("images/image.jpg");
+            _PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            _PictureBox2.Load("images/image.jpg");
+            _PictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            _ProgressBar.Hide();
+        }
+
+        private void _ButtonImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                pathToImage = fileDialog.FileName;
+            }
+
+            _PictureBox1.Load(pathToImage);
+        }
+
+        private void _ButtonDithering_Click(object sender, EventArgs e)
+        {
+            ditheringImageThread = new Thread(DitheringImage);
+            ditheringImageThread.Start();
+        }
+    }
+}
